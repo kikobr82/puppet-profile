fc7c2fb72eb8b5f15d3752936d55d89c242d33f2
root <root@kikobr821.mylabserver.com>
2017-07-11 17:35:27 +0000

    Commit 2017071126 for folder profile

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,35 +1,15 @@
-#class profile::apache(
-#  Boolean $default_vhost = false,
+class profile::apache(
+  Boolean $default_vhost = false,
+  Hash $apache_vhost_servers,
 #  Integer $port          = 80,
-#  String $docroot        = '/var/www',
-#) {
-#  class { '::apache':
-#    default_vhost => $default_vhost,
-#  }
+#  String $docroot        = '/var/www/test',
+) {
+  class { '::apache':
+    default_vhost => $default_vhost,
+  }
+  create_resources(::apache::vhost, $apache_vhost_servers)
 #  ::apache::vhost { 'kikobr822.mylabserver.com':
 #    port    => $port,
 #    docroot => $docroot,
 #  }
-#  file {'/var/www/index.html':
-#    ensure  => file,
-#    content => 'Teste Page. This is a test',
-#  }
-#}
-
-class profile::apache {
-  class { '::apache':
-    default_vhost => true,
-  }
-
-  $docroot = "/var/www/${facts['fqdn']}"
-
-  ::apache::vhost { $facts['fqdn']:
-    port    => 80,
-    docroot => $docroot,
-    before  => File["${docroot}/index.html"],
-  }
-
-  file {  "${docroot}/index.html":
-    content => "Test pageThis is a test",
-  }
 }
